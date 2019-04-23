cbd09f4ba4d563d6b4e620d7f610d0c2b799fc31
ascotttoney <ascotttoney@gmail.com>
2019-04-23 16:50:39 -0400

    attempt 1

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,41 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+# iterate over the response hash to find the collection of `films` for the given
+#   `character`
+# collect those film API urls, make a web request to each URL to get the info
+#  for that film
+# return value of this method should be collection of info about each film.
+#  i.e. an array of hashes in which each hash reps a given film
+# this collection will be the argument given to `print_movies`
+#  and that method will do some nice presentation stuff like puts out a list
+#  of movies by title. Have a play around with the puts with other info about a given film.
+
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get('http://www.swapi.co/api/people')
   response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  # movies = []
+  # binding.pry
+
+  response_hash['results'].each do |character|
+    if character["name"].downcase == character_name
+      return response_hash["results"][0]["films"]
+    end
+  end
 end
+
+# get_character_movies_from_api("Luke Skywalker")
+
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |title|
+    response_title = RestClient.get("#{title}")
+    title_hash = JSON.parse(response_title)
+    puts title_hash["title"]
+  end
 end
 
 def show_character_movies(character)
